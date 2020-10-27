57431df6270ff5583eb7edd07529b9213bef7490
Aaron-ante <araaanteca@ittepic.edu.mx>
2020-10-27 16:26:24 -0700

    pila

---
+++ src/Pila.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aaron
+ */
+public class Pila {
+    
+    private  int  data[] ; // tipo object ;
+    private int tope;
+    private int x;
+
+  
+   
+  public int getTope(){
+      x = data[tope];
+      return x;
+  }
+    
+    
+    
+    public Pila(int valor){
+       data = new int[valor];
+       tope = -1;
+    }
+   
+    
+    
+   
+    
+    
+    public boolean Insertar( int valor){
+        if(tope == 6){				      
+        return false; 
+    }						      
+         tope++;						      
+   	 data[tope] = valor;
+         return true;
+    }
+    
+    public boolean  Eliminar(){
+       int x;
+    if( tope == -1){				      
+      	 return false;						      
+    }
+       
+        x = data[ tope ];
+        tope--;
+        return true;
+    }
+
+   
+   
+
+   
+    
+}
